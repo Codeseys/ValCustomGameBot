185c5717614f539cfe162bc8f2318d25885687a7
Baladithya Balamurugan <bbaladithya@gmail.com>
2020-09-01 15:15:13 -0700

    REEEEEEEEEEee

--- bottestvc.py
+++ bottestvc.py
@@ -11,15 +11,13 @@ load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 client = discord.Client()
 
-<<<<<<< HEAD
-=======
 messageID = None
 
 
->>>>>>> 9d1f6c5ad88bad8b8f34014c68613cd44f56fb24
 @client.event
 async def on_ready():
     print('Logged on as {0}!'.format(client.user))
+
 
 @client.event
 async def on_reaction_add(reaction, user):
@@ -32,12 +30,9 @@ async def on_reaction_add(reaction, user):
             users = await reaction.users().flatten()
             await reaction.message.channel.send('<@{0}>'.format(user.id))
             print(users)
-<<<<<<< HEAD
         # elif str(reaction.emoji) ==
-=======
             print(messageID.id)
 
->>>>>>> 9d1f6c5ad88bad8b8f34014c68613cd44f56fb24
 
 @ client.event
 async def on_message(message):
@@ -50,23 +45,20 @@ async def on_message(message):
         await message.channel.send(content=None, embed=embed)
     elif message.content.startswith('+'):
         # print(message.content)
-<<<<<<< HEAD
         await message.channel.send("Moving...")
-        
-=======
+
         messageID = await message.channel.send("Moving...")
         print(messageID.id)
->>>>>>> 9d1f6c5ad88bad8b8f34014c68613cd44f56fb24
 
     elif message.content == "!logoff":
         await client.logout()
         exit()
 
+
 async def on_reaction_remove(reaction, user):
     if user != client.user:
         if str(reaction.emoji) == "<:valorant:718711817911664650>":
-            print( "removed" )
-            
+            print("removed")
 
 
 client.run(TOKEN)
