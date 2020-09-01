b51d534800ef5ba340cc6bec5bde94aa1d2b5532
Kratos098 <kratos.github@gmail.com>
2020-09-01 15:12:10 -0700

    nothing

--- bottestvc.py
+++ bottestvc.py
@@ -11,22 +11,21 @@ load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 client = discord.Client()
 
-
 @client.event
 async def on_ready():
     print('Logged on as {0}!'.format(client.user))
 
-
 @client.event
 async def on_reaction_add(reaction, user):
     print(str(reaction.emoji))
+
     if user != client.user:
         if str(reaction.emoji) == "<:valorant:718711817911664650>":
             print(client.user, user.name, user.id)
             users = await reaction.users().flatten()
             await reaction.message.channel.send('<@{0}>'.format(user.id))
             print(users)
-
+        # elif str(reaction.emoji) ==
 
 @ client.event
 async def on_message(message):
@@ -39,9 +38,17 @@ async def on_message(message):
     elif message.content.startswith('+'):
         # print(message.content)
         await message.channel.send("Moving...")
+        
 
     elif message.content == "!logoff":
         await client.logout()
         exit()
+
+async def on_reaction_remove(reaction, user):
+    if user != client.user:
+        if str(reaction.emoji) == "<:valorant:718711817911664650>":
+            print( "removed" )
+            
+
 
 client.run(TOKEN)
